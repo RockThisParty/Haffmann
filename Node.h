0e22dbf6bae7d6b5005a799a9c2d17c05fc7a468
RockThisParty <alamovvladimir@gmail.com>
2017-04-23 14:37:39 +0800

    Add files via upload

---
+++ Node.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <fstream>
+
+using namespace std;
+
+class Node
+{
+public:
+	int a;
+	char c;
+	Node *left_child, *right_child;
+
+	Node()
+	{
+		left_child = right_child = NULL;
+	}
+
+	Node(Node *L, Node *R)
+	{
+		left_child = L;
+		right_child = R;
+		a = L->a + R->a;
+	}
+};
---
+++ Table.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Node.h"
+
+class MyCompare
+{
+public:
+	bool operator()(const Node* l, const Node* r) const
+	{
+		return l->a < r->a;
+	}
+};
+
+
+vector<bool> code;
+map<char, vector<bool> > table;
+
+void BuildTable(Node *root)
+{
+	if (root->left_child != NULL)
+	{
+		code.push_back(0);
+		BuildTable(root->left_child);
+	}
+
+	if (root->right_child != NULL)
+	{
+		code.push_back(1);
+		BuildTable(root->right_child);
+	}
+
+	//if(root->c)
+	if (root->left_child == NULL && root->right_child == NULL)
+		table[root->c] = code;
+
+	code.pop_back();
+}
